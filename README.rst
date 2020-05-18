0c8ea6f5e9330bb49f47b2a6ab67111f5317a2e8
Zack Buhman <zack@buhman.org>
2020-05-17 21:13:03 -0700

    README: add usage
    This also makes example_client.py a bit more ergonomic for casual
    non-development usage.

--- README.rst
+++ README.rst
@@ -3,3 +3,47 @@ apocrypha
 #########
 
    works, usually written, of unknown authorship or of doubtful origin
+
+Usage
+=====
+
+``example_client.py`` is the current reference implementation of the "native"
+client protocol. It takes local filesystem paths as arguments, and uploads each
+to ptpb.io (a public deployment of apocrypha).
+
+.. code::
+
+   $ sha256sum test.png test.webm
+   d2f1543b82d8219b0c2086224339bf503211a221a2ec55a9a39f23bf8a1e2481  test.png
+   ba0f0e2ba50ccea0a80f114d0e0a40595cb2922bdf01f00ce7134405b23a2302  test.webm
+   $ python test/example_client.py test.png test.webm
+   test.png https://ptpb.io:667/d2f1
+   test.webm https://ptpb.io:667/ba0f
+
+Verify that apocrypha is able to regurgitate these:
+
+.. code::
+
+   $ curl -s -D/dev/stderr https://ptpb.io:667/d2f1.png | sha256sum
+   HTTP/1.1 200
+   transfer-encoding: chunked
+   content-type: image/png
+
+   d2f1543b82d8219b0c2086224339bf503211a221a2ec55a9a39f23bf8a1e2481  -
+   $ curl -s -D/dev/stderr https://ptpb.io:667/ba0f.webm | sha256sum
+   HTTP/1.1 200
+   transfer-encoding: chunked
+   content-type: video/webm
+
+   ba0f0e2ba50ccea0a80f114d0e0a40595cb2922bdf01f00ce7134405b23a2302  -
+
+The ``content-type`` feature makes apocrypha suitable for presenting common
+media types (plaintext, html, video, etc..) in web browsers.
+
+Prefix length
+=============
+
+apocrypha has a concept of a "prefix_length", specified by the client. An
+uploaded file is addressable by **both** the full 64 character sha256 hex digest
+and a prefix of that hex digest of length exactly equal to the "prefix_length"
+specified at upload time.
--- test/example_client.py
+++ test/example_client.py
@@ -5,8 +5,9 @@ import ssl
 import struct
 import sys
 
-host = "localhost"
+host = "ptpb.io"
 port = 26708
+http_port = 667
 
 ssl_context = ssl.create_default_context()
 ssl_context.check_hostname = False
@@ -18,21 +19,25 @@ with socket.create_connection((host, port)) as sock, \
     for filename in sys.argv[1:]:
         digest = hashlib.sha256()
 
-        # request a prefix length of 3
-        ssl_sock.write(struct.pack("!B", 3))
+        # request a prefix length of 4
+        prefix_length = 4
+        ssl_sock.write(struct.pack("!B", prefix_length))
 
         with open(filename, "rb") as f:
-            while buf := f.read(1024 * 32):
+            while buf := f.read(1024 * 64 - 4):
                 ssl_sock.write(struct.pack("!L", len(buf)))
                 ssl_sock.write(buf)
                 digest.update(buf)
-                # tell the server we have finished transmitting the file
+            # tell the server we have finished transmitting the file
             ssl_sock.write(struct.pack("!L", 0))
 
         remote_length, = struct.unpack("!L", ssl_sock.read(4))
         remote_value = ssl_sock.read(remote_length)
         assert(len(remote_value) == remote_length)
-        print(filename)
-        print("  local: ", digest.hexdigest())
-        print(" remote: ", binascii.hexlify(remote_value).decode("utf-8"))
+        url = "https://" + host + ":" + str(http_port) + "/" \
+              + binascii.hexlify(remote_value).decode("utf-8")[:prefix_length]
+        print(filename, url)
+
+        #print("  local: ", digest.hexdigest())
+        #print(" remote: ", binascii.hexlify(remote_value).decode("utf-8"))
         # we can either close the connection here, or start sending a new file
