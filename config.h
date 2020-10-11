70ed75a1ed237ca113b9205c6accf194028d76d3
Zack Buhman <zack@buhman.org>
2020-10-11 03:59:57 -0700

    config.h: uacme

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
-#define NATIVE_PORT 0
-#define HTTP_PORT 0
+#define NATIVE_PORT 444
+#define HTTP_PORT 443
 
 #define TLS_CA "/etc/ssl/cert.pem"
-#define TLS_CERT "cert.pem"
-#define TLS_KEY "key.pem"
+#define TLS_CERT "/mnt/uacme/ptpb.io/cert.pem"
+#define TLS_KEY "/mnt/uacme/private/ptpb.io/key.pem"
