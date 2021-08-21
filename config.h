ab34a96cc49151e5573924727e3320b5df2a25c4
Zack Buhman <zack@buhman.org>
2021-08-21 12:38:02 -0700

    config.h: defaults

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
-#define NATIVE_PORT 444
-#define HTTP_PORT 443
+#define NATIVE_PORT 4444
+#define HTTP_PORT 4443
 
 #define TLS_CA "/etc/ssl/cert.pem"
-#define TLS_CERT "/mnt/uacme/ptpb.io/cert.pem"
-#define TLS_KEY "/mnt/uacme/private/ptpb.io/key.pem"
+#define TLS_CERT "localhost.crt.pem"
+#define TLS_KEY "localhost.key.pem"
